97185971d11c4db3cd17ff8947a5ce85b5243d71
Chao Wang <charleswang@ChaodeMacBook-Pro.local>
2020-04-13 14:15:53 -0700

    Updates

---
+++ precache-manifest.80fef307c3f839a89467d470d252367f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46ca82216696e473871fa61b69df2dce",
+    "url": "/myBlog/index.html"
+  },
+  {
+    "revision": "1b8e44ff0590d8b089b8",
+    "url": "/myBlog/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b482b38991d75429e67",
+    "url": "/myBlog/static/js/2.81bf6db7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myBlog/static/js/2.81bf6db7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b8e44ff0590d8b089b8",
+    "url": "/myBlog/static/js/main.dc7448b0.chunk.js"
+  },
+  {
+    "revision": "087aa0f77a30363321e6",
+    "url": "/myBlog/static/js/runtime-main.3a72c7cb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myBlog/static/media/logo.5d5d9eef.svg"
+  }
+]);
